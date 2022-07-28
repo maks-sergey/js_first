d07ec047ff1395aed97d6f81352c175d023ddaf5
Maximenko <maks.sergey.11@gmail.com>
2022-07-28 19:35:05 +0300

    continie practice

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
 const personalMuvieDB = {
@@ -8,12 +10,28 @@ const personalMuvieDB = {
     privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов:', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов:', ''),
-      d = prompt('На сколько оцените его?', '');
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько оцените его?', '');
 
-personalMuvieDB.movies[a] = b;
-personalMuvieDB.movies[c] = d;
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMuvieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+    
+}   
+
+if (personalMuvieDB.count < 10) {
+    console.log('Просмотрено довольно мало фильмов');
+} else if (personalMuvieDB.count >= 10 && personalMuvieDB < 30) {
+    console.log('Вы классический зритель');
+} else if (personalMuvieDB.count >= 30) {
+    console.log ('Вы киноман');
+} else {
+    console.log('Error');
+}
 
 console.log(personalMuvieDB);
